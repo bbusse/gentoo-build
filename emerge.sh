37ac22a18abe0a8b4fcd322f85b0baa0b260ff1e
Björn Busse <bj.rn@baerlin.eu>
2025-12-07 03:57:21 +0100

    Parallelize emerge

--- emerge.sh
+++ emerge.sh
@@ -6,11 +6,21 @@ main() {
     local target_flavour
     target_flavour="${1}"
 
+    # Parallelization knobs (override via env): EMERGE_JOBS/EMERGE_LOAD and MAKEOPTS
+    local emerge_jobs
+    emerge_jobs="${EMERGE_JOBS:-$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 2)}"
+    local emerge_load
+    emerge_load="${EMERGE_LOAD:-${emerge_jobs}}"
+    # Respect existing MAKEOPTS, else set a sane default
+    export MAKEOPTS="${MAKEOPTS:--j${emerge_jobs}}"
+
     printf "Building gentoo flavour: %s\n" "${target_flavour}"
 
     case "${target_flavour}" in
     containeros)
-        emerge -qv \
+         emerge -qv \
+             --jobs "${emerge_jobs}" \
+             --load-average "${emerge_load}" \
                --buildpkg \
                --buildpkg-exclude \
                "virtual/* \
@@ -20,7 +30,9 @@ main() {
                @net
         ;;
     sway)
-        emerge -qv \
+         emerge -qv \
+             --jobs "${emerge_jobs}" \
+             --load-average "${emerge_load}" \
                --buildpkg \
                --buildpkg-exclude \
                "virtual/* \
